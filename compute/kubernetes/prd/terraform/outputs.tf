bf227ce2925ec8fcf0aa36058410ef087c59dd8a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-04-10 00:56:11 +0100

    Add SSH tunnel command output to Terraform configuration

--- compute/kubernetes/prd/terraform/outputs.tf
+++ compute/kubernetes/prd/terraform/outputs.tf
@@ -15,3 +15,8 @@ output "kubeconfig_command" {
 	description = "OCI CLI command to generate the kubeconfig for this cluster."
 	value = "oci ce cluster create-kubeconfig --cluster-id ${oci_containerengine_cluster.cluster.id} --region ${var.region}"
 }
+
+output "ssh_tunnel_command" {
+	description = "SSH command to create a tunnel to the API endpoint of the cluster."
+	value = "ssh -N -L 6443:${oci_containerengine_cluster.cluster.endpoints[0]}:6443 ubuntu@go-prd-jumpserver.tmlmobilidade.pt"
+}

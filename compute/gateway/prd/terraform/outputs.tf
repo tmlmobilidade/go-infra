5f754196a5a0d52550daa8cd8e62a5280c170d09
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-04-11 00:34:37 +0100

    fix: update outputs to reference gateway instance instead of jumpserver

--- compute/gateway/prd/terraform/outputs.tf
+++ compute/gateway/prd/terraform/outputs.tf
@@ -3,10 +3,10 @@
 
 output "instance_private_ip" {
 	description = "Private IP address of the JumpServer instance."
-	value = oci_core_instance.jumpserver.private_ip
+	value = oci_core_instance.gateway.private_ip
 }
 
 output "ssh_command" {
 	description = "SSH command to connect to the JumpServer instance."
-	value = "ssh -i <private-key-path> ubuntu@${oci_core_instance.jumpserver.public_ip}"
+	value = "ssh -i <private-key-path> ubuntu@${oci_core_instance.gateway.public_ip}"
 }

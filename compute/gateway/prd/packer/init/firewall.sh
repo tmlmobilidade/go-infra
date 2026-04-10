24101fd6e2e0d0ac0fe3112eb705de7e421d860b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-04-11 00:20:34 +0100

    Add packer

---
+++ compute/gateway/prd/packer/init/firewall.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# 1.
+# Open Clickhouse's default ports `8123`, `9000`, `9444`, `2181` in the firewall
+# to allow external connections from within the private subnet. This is necessary
+# for the cluster to function properly, as the nodes need to communicate with each
+# other and with clients. The firewall appears to block incoming connections
+# by default, so we need to explicitly allow it.
+
+echo "[firewall] Clearing restrictive iptables rules..."
+sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+sudo iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
+echo "[firewall] iptables rules cleared."

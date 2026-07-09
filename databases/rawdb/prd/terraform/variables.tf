6afd66543f5c38270509f7bab1ad551f0d2d6805
Nuno Sponsor <nuno.patrocinio432@gmail.com>
2026-07-09 11:42:48 +0100

    fix: change instance to rawdb name

--- databases/rawdb/prd/terraform/variables.tf
+++ databases/rawdb/prd/terraform/variables.tf
@@ -4,7 +4,7 @@
 variable "display_name" {
 	type = string
 	description = "The name of the deployment. Used as the display name for resource names and tags."
-	default = "iso-go-prd-mongodb"
+	default = "iso-go-prd-rawdb"
 }
 
 variable "instance_count" {

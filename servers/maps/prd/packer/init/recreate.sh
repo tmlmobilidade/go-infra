b8ad1395cac55db73dcf29907254c5424b81b77b
Nuno Sponsor <nuno.patrocinio432@gmail.com>
2026-08-17 14:52:51 +0100

    fix: update docker compose command in recreate.sh to include tileserver

--- servers/maps/prd/packer/init/recreate.sh
+++ servers/maps/prd/packer/init/recreate.sh
@@ -31,7 +31,7 @@ while true; do
 	fi
 	echo "Updated files."
 	echo "Recreating planetiler...";
-	docker compose -f /opt/app/compose.yaml up -d --build --force-recreate --remove-orphans --pull=always planetiler
+	docker compose -f /opt/app/compose.yaml up -d --build --force-recreate --remove-orphans --pull=always planetiler tileserver
 	echo "Done! Planetiler recreated.";
 	sleep 86400; # Sleep for 24 hours
 done;

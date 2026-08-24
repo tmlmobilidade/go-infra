ee57ab05ed5669f292c216deb8da6d1d46d97bd0
Nuno Sponsor <nuno.patrocinio432@gmail.com>
2026-08-24 14:33:58 +0100

    chore: update VM resource configurations in Terraform variables to increase OCPUs from 2 to 8 and memory from 12GB to 32GB in godb stg

--- databases/godb/stg/terraform/variables.tf
+++ databases/godb/stg/terraform/variables.tf
@@ -114,13 +114,13 @@ variable "vm_shape" {
 variable "vm_ocpus" {
 	type = number
 	description = "Number of OCPUs per replica VM."
-	default = 2
+	default = 8
 }
 
 variable "vm_memory_in_gbs" {
 	type = number
 	description = "Memory in GBs per replica VM."
-	default = 12
+	default = 32
 }
 
 variable "boot_volume_size_in_gbs" {
